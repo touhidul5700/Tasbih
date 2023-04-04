c96dc21c48172c225b72933bcf6cd356ac714cdb
MD. TOUHIDUL ISLAM SHADHIN <123452416+touhidul77@users.noreply.github.com>
2023-04-04 16:20:02 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ let i = 0;
 /* count btn */
 document.getElementById("btn").addEventListener("click",function(){
     i=i+1;
-    console.log(i);
     document.getElementById("result").innerHTML = i;
 })
 /* rest btn */
 document.getElementById("reset").addEventListener("click",function(){
     i = 0;
-    console.log(i);
     document.getElementById("result").innerHTML = i;
 })
